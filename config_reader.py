70877f0173bddb756cf9312958718d807a2903fd
Jorik Kornev <jorikkornev@gmail.com>
2024-11-14 20:22:54 +0300

    Add files via upload
    (cherry picked from commit d9f89efa61ead5e7a6e6f15ac8f52b4cca09720b)

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
-
 class Settings(BaseSettings):
     # Желательно вместо str использовать SecretStr
     # для конфиденциальных данных, например, токена бота
@@ -12,7 +11,6 @@ class Settings(BaseSettings):
     # В данном случае будет использоваться файла .env, который будет прочитан
     # с кодировкой UTF-8
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-
 
 # При импорте файла сразу создастся
 # и провалидируется объект конфига,
